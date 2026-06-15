21308752d3e2289a4173536171a71a1213845f7c
Paweł <pawel@msd.info.pl>
2026-06-15 09:53:03 +0200

    WPF

---
+++ WpfApp/Models/Item.Methods.cs
@@ -0,0 +1,12 @@
+﻿namespace WpfApp.Models
+{
+    //partial - pozwala podzielić definicję klasy na wiele plików, co jest przydatne w dużych projektach lub przy generowaniu kodu.
+    //wymagania: wszystkie części klasy muszą być oznaczone jako partial, muszą mieć tę samą nazwę i znajdować się w tym samym namespace oraz tym samym assembly.
+    internal partial class Item
+    {
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Description: {Description}, Price: {Price:C}");
+        }
+    }
+}
---
+++ WpfApp/Models/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace WpfApp.Models
+{
+    internal partial class Item
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
