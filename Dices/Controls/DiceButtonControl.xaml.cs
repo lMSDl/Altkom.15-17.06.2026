f0e2a2c5218d9c9ccc530ae5c0bc21e167f906ea
Paweł <pawel@msd.info.pl>
2026-06-16 14:55:27 +0200

    DependencyProperty

---
+++ Dices/Controls/DiceButtonControl.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dices.Controls
+{
+    /// <summary>
+    /// Interaction logic for DiceButtonControl.xaml
+    /// </summary>
+    public partial class DiceButtonControl : Button
+    {
+        public DiceButtonControl()
+        {
+            InitializeComponent();
+        }
+
+        public static DependencyProperty NumberProperty = DependencyProperty.Register(
+            nameof(Number),
+            typeof(int),
+            typeof(DiceButtonControl));
+
+        public int Number
+        {
+            get => (int)GetValue(NumberProperty);
+            set => SetValue(NumberProperty, value);
+        }
+
+        public static DependencyProperty IsBlockedProperty = DependencyProperty.Register(
+            nameof(IsBlocked),
+            typeof(bool),
+            typeof(DiceButtonControl));
+
+        public bool IsBlocked
+        {
+            get => (bool)GetValue(IsBlockedProperty);
+            set => SetValue(IsBlockedProperty, value);
+        }
+
+    }
+}
